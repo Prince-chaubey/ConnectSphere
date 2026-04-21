f433fa076e5636c903fc86a7f4a7a1da354c5ab5
Yash Raj <yash26988412@gmail.com>
2026-04-21 16:05:03 +0530

    Fix the Next deployment

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const projectRouter = require("./routes/projectRoute");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
